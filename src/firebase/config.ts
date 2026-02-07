383756eef9fc78cebd4f75a60143d7542e600b3d
Abel <abelnono1994@gmail.com>
2026-02-07 19:16:49 +0000

    (Nom du projet Inoubliable ID du projet studio-8835251637-962db Numéro d

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyD-gwoe5FgX7gZB6tpbzi37wpkJgeGW4Lw",
   authDomain: "studio-8835251637-962db.firebaseapp.com",
   projectId: "studio-8835251637-962db",
-  storageBucket: "studio-8835251637-962db.appspot.com",
+  storageBucket: "studio-8835251637-962db.firebasestorage.app",
   messagingSenderId: "192229525992",
   appId: "1:192229525992:web:29acab7a7337eb2515fc5d"
 };
