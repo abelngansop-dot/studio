c072f537a8cb8b7b793359a1686089f6e445d354
Abel <abelnono1994@gmail.com>
2026-02-07 19:12:41 +0000

    Il est très important de sécuriser le côté administrateur pour que n'imp

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-8835251637-962db",
-  "appId": "1:192229525992:web:4521a5e8f0ffd9c115fc5d",
-  "apiKey": "AIzaSyCkZlPtpenptrXfIfx5i9o9Nk3WHlXZ6yM",
-  "authDomain": "studio-8835251637-962db.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "192229525992"
+  apiKey: "AIzaSyD-gwoe5FgX7gZB6tpbzi37wpkJgeGW4Lw",
+  authDomain: "studio-8835251637-962db.firebaseapp.com",
+  projectId: "studio-8835251637-962db",
+  storageBucket: "studio-8835251637-962db.appspot.com",
+  messagingSenderId: "192229525992",
+  appId: "1:192229525992:web:29acab7a7337eb2515fc5d"
 };
